3d98906d61bec6bc5cf18d5e8ac6e5a4ae8014b1
Amrit Nayak <amritnayak48@gmail.com>
2021-07-10 20:56:53 +0530

    Add more functionality and styling

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 const cards = document.querySelectorAll(".card-item");
+let cardCount = 0;
 let pause = false;
-
 let isFirstFlipped = false;
 let firstCard, secondCard;
 
-// Add event listeners for flipping each card item
-cards.forEach((card) => {
-  card.addEventListener("click", flipCard);
-});
+function startGame() {
+  reset();
+
+  // Add event listeners for flipping each card item
+  cards.forEach((card) => {
+    card.addEventListener("click", flipCard);
+  });
+}
 
 function flipCard() {
   if (pause) return;
@@ -27,10 +31,18 @@ function flipCard() {
 
 // Check if cards match
 function isMatch() {
+  countMoves();
   firstCard.dataset.name === secondCard.dataset.name ? cardsMatch() : cardsUnmatch();
 }
 
+let status = document.querySelector(".status");
 function cardsMatch() {
+  cardCount += 2;
+  if (cardCount == cards.length) {
+    clearInterval(interval);
+    status.innerHTML += " - You Won!";
+  }
+
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
 
@@ -50,3 +62,59 @@ function cardsUnmatch() {
     pause = false;
   }, 1500);
 }
+
+function shuffleCards() {
+  cards.forEach((card) => {
+    card.classList.remove("flip");
+    let randomPosition = Math.floor(Math.random() * cards.length);
+    card.style.order = randomPosition;
+  });
+}
+
+// Timer
+let timer = document.querySelector(".timer");
+var seconds = 0,
+  minutes = 0;
+var interval;
+
+function startTimer() {
+  interval = setInterval(() => {
+    if (seconds == 60) {
+      minutes++;
+      seconds = 0;
+    }
+    timer.innerHTML = minutes + " mins " + seconds++ + " secs";
+  }, 1000);
+}
+
+// Moves Counter
+let movesCounter = document.querySelector(".moves");
+let moves = 0;
+
+function countMoves() {
+  moves++;
+  movesCounter.innerHTML = moves + " moves";
+
+  if (moves == 1) {
+    seconds = 0;
+    minutes = 0;
+    startTimer();
+  }
+}
+
+// Reset
+const resetButton = document.querySelector(".reset");
+
+resetButton.addEventListener("click", startGame);
+
+function reset() {
+  timer.innerHTML = "";
+  movesCounter.innerHTML = "";
+  cardCount = seconds = minutes = moves = 0;
+  shuffleCards();
+  clearInterval(interval);
+  pause = isFirstFlipped = false;
+  firstCard = secondCard = null;
+}
+
+startGame();
